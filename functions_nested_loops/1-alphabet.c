bd8ea0efcc6a2f7a34b2356dff881d98edd2e2eb
KillianRipoche <killian.ripoche@gmail.com>
2024-10-24 11:45:35 +0000

    test1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - rgfvrtgtrggt
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 97; a <= 122; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
