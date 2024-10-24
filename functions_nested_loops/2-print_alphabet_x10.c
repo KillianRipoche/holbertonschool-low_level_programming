21a5f2de2775be3b6b493f1719fe5e5959baa4a7
KillianRipoche <killian.ripoche@gmail.com>
2024-10-24 12:33:53 +0000

    test2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -9,13 +9,13 @@ void print_alphabet_x10(void)
 	char a;
 	int n = 48;
 
-	while (48 <= 57)
+	while (n <= 57)
 	{
 		for (a = 97; a <= 122; a++)
 		{
 			_putchar(a);
 		}
-		n++;
 		_putchar('\n');
+		n++;
 	}
 }
