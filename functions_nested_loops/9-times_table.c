a63ea219544d75f2a0d51f748e00026e8f9cc9f3
KillianRipoche <killian.ripoche@gmail.com>
2024-10-25 13:45:17 +0000

    test1

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * times_table
+ *
+ * Return: Always 0
+ */
+void times_table(void)
+{
+	int n;
+	int mult;
+	int prod;
+
+	for (n = 0; n <= 9; n++)
+	{
+		_putchar('0');
+
+		for (mult = 1; mult <= 9; mult++)
+		{
+			prod = n * mult;
+			_putchar(',');
+			_putchar(' ');
+			if (prod <= 9)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((prod / 10) + '0');
+			}
+			_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
