4e9f91d07c974ce8e98edcb9701323313ef848dc
KillianRipoche <killian.ripoche@gmail.com>
2024-10-25 09:27:17 +0000

    test1

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+#include <inttypes.h>
+/**
+ * _abs - Entry
+ * @j: abs checked
+ * Return: Always 0
+ */
+int _abs(int j)
+{
+	if (j >= 0)
+	{
+		return (j);
+	}
+	return (-j);
+}
+
