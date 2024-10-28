f05c1270261cd65d3cfab8e563243034d831e900
KillianRipoche <killian.ripoche@gmail.com>
2024-10-28 09:30:04 +0000

    test1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <ctype.h>
+#include <stdio.h>
+/**
+ * _isdigit - Entry
+ * @c: checker
+ * Return : Always 0
+ */
+int _isdigit(int c)
+{
+	if (isdigit(c))
+		return (1);
+	else
+		return (0);
+
+	return (0);
+}
