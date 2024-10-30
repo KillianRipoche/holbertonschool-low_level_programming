e6f52e77fa338116dcc17c347f2f1d4b66f4f7c1
KillianRipoche <killian.ripoche@gmail.com>
2024-10-30 13:29:15 +0000

    test1

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,10 +1,25 @@
-#include <stdio.h>
 #include "main.h"
+#include "2-strlen.c"
 /**
- *
- *
+ * rev_string - entry
+ * @s: reverse
  *
  */
 void rev_string(char *s)
 {
+	int a, b;
+	char temp;
 
+	a = 0;
+	b = _strlen(s) - 1;
+
+	while (a < b)
+	{
+		temp = s[a];
+		s[a] = s[b];
+		s[b] = temp;
+
+		a++;
+		b--;
+	}
+}
