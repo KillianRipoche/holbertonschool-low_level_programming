96f9ff2bffce3a062b5f7a8677cfdc64bc87f8be
KillianRipoche <killian.ripoche@gmail.com>
2024-10-31 13:35:30 +0000

    Concatenation" git push

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -2,21 +2,22 @@
 #include <stdio.h>
 #include "2-strlen.c"
 /**
- * *_strcat - entry
+ * _strcat - entry
  * @dest: chaine
  * @src: ajout
+ * Return: dest
  */
-char *_strcat( char *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
 	int i, j;
 
-	for (i = 0; dest[i] != '\0'; i++);
+	for (i = 0; dest[i] != '\0';)
+		i++;
 
 	for (j = 0; src[j] != '\0'; j++, i++)
 	{
 		dest[i] = src[j];
 	}
-
 	dest[i] = '\0';
 	return (dest);
 }
