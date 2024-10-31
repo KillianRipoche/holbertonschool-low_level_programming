9fa71cfe801d492fc4f6dfcb937a3e62ba6c4266
KillianRipoche <killian.ripoche@gmail.com>
2024-10-31 13:16:24 +0000

    concatenates two strings

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include "2-strlen.c"
+/**
+ * *_strcat - entry
+ * @dest: chaine
+ * @src: ajout
+ */
+char *_strcat( char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++);
+
+	for (j = 0; src[j] != '\0'; j++, i++)
+	{
+		dest[i] = src[j];
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
