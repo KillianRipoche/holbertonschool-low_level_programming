9051961941599402beb965cc45aa3e22ce695bf7
KillianRipoche <killian.ripoche@gmail.com>
2024-11-06 10:14:25 +0100

    recursion

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts_recursion - Entry
+ * @s: chaine de caractere
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar('\n');
+
+	else
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+	}
+}
