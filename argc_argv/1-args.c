748dabfc50ea9e24c78b785d479af8dcb524941f
KillianRipoche <killian.ripoche@gmail.com>
2024-11-07 21:31:12 +0100

    argc

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * @argc: int
+ * @argv: valeur
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)(argv);
+	printf("%d\n", (argc - 1));
+
+	return (0);
+}
