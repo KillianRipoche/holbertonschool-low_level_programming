cb2cc8656e573eb5b3cf9b38b53245885a30c5f9
KillianRipoche <killian.ripoche@gmail.com>
2024-11-15 09:37:59 +0100

    fix betty

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -11,9 +11,11 @@ char *str_concat(char *s1, char *s2)
 {
 	int i = 0, j = 0, k = 0;
 	char *ptr;
-	
-	if ((s1 == NULL) && (s2 == NULL))
-		s1 = "", s2 = "";
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	while (s1[i])
 		i++;
 	while (s2[j])
