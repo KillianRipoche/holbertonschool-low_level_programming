d488749023756ec721ee31e3200b3c61d9b4d78f
KillianRipoche <killian.ripoche@gmail.com>
2024-11-20 10:29:41 +0100

    fix betty

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,15 +1,14 @@
-#include "function_pointers.h"
+#include <stdlib.h>
 /**
- * print_name - Entry
- * @name: nom a afficher
- * @f: pointeur de fonction
- * Return: Void
+ * print_name - a function prints a name
+ * @name: name of function
+ * @f: pointer of function
+ * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (f == 0)
-	{
-		return(NULL);
-	}
+	if (f == NULL)
+		return;
+
 	f(name);
 }
