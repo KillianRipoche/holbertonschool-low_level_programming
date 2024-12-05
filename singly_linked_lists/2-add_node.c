cb73b95a07252ce5e6261546b2e969244c1468ba
KillianRipoche <killian.ripoche@gmail.com>
2024-12-05 18:38:53 +0100

    fix betty

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -11,15 +11,23 @@ list_t *add_node(list_t **head, const char *str)
 	unsigned int length = 0;
 
 	if (head == NULL || new_node == NULL)
+	{
 		return (NULL);
+	}
+
 
 	new_node->str = strdup(str);
 
 	while (str[length])
+	{
 		length++;
+	}
+
 
 	new_node->len = length;
+
 	new_node->next = *head;
+
 	*head = new_node;
 
 	return (new_node);
