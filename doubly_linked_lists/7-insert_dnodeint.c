e5699cc8171a0a6109b4b81b0d1c0303102f42d1
KillianRipoche <killian.ripoche@gmail.com>
2024-12-05 13:04:11 +0100

    add null for head

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -10,12 +10,13 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_node, *tempo = *h;
-	unsigned int i;
+	unsigned int i = 0;
 
 	new_node = malloc(sizeof(dlistint_t));
+	if (h == NULL)
+		return(NULL);
 	if (new_node == NULL)
 		return (NULL);
-
 	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = NULL;
