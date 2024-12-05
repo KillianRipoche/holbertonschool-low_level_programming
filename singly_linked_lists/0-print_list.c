e06743e9f87d5ed68fc485b143d405a3e860fb0d
KillianRipoche <killian.ripoche@gmail.com>
2024-12-05 18:19:29 +0100

    fix betty

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,26 +1,25 @@
 #include "lists.h"
+#include <stdio.h>
+#include <string.h>
 /**
  * print_list - Entry
  * @h: head
- * Return: nodes
+ * Return: the number of nodes
  */
+
 size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
+	size_t number_node = 0;
 
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-		{
 			printf("[0] (nil)\n");
-		}
 		else
-		{
 			printf("[%u] %s\n", h->len, h->str);
-		}
 
 		h = h->next;
-		nodes++;
+		number_node++;
 	}
-	return (nodes);
+	return (number_node);
 }
