8bb57a567571fff6e20b1682eea8194253be2d2e
KillianRipoche <killian.ripoche@gmail.com>
2024-12-05 18:26:42 +0100

    fix betty

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -9,17 +9,18 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node = malloc(sizeof(list_t));
+	list_t *new_node;
 	unsigned int len = 0;
-
-	if (head == NULL || new_node == NULL)
-		return (NULL);
-
-	new_node->str = strdup(str);
 
 	while (str[len])
 		len++;
 
+	new_node = malloc(sizeof(list_t));
+
+	if (!new_node)
+		return (NULL);
+
+	new_node->str = strdup(str);
 	new_node->len = len;
 	new_node->next = *head;
 	*head = new_node;
