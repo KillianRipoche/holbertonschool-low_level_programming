1c6c71b7b38dbf02186779f01a26648fcf8ec932
KillianRipoche <killian.ripoche@gmail.com>
2024-12-05 11:07:11 +0100

    fix betty

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,5 +1,11 @@
 #include "lists.h"
-/** */
+/**
+ * add_dnodeint_end - Entry
+ *
+ * @head: tete
+ * @n: const int
+ * @Return: new_node
+ */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node, *tempo;
