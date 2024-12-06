9145fb0999bd644b757655537e528f6180421f22
KillianRipoche <killian.ripoche@gmail.com>
2024-12-06 15:42:16 +0100

    essai n1

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * append_text_to_file - Entry
+ * @filename: file add
+ * @text_content: texte a ajouter
+ * Return: 1
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd = 0; /* file descriptor */
+	int lg; /* longueur de text content */
+	int add_var;
+
+	if (filename == NULL)
+		return(-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		return (-1);
+
+	while (text_content[lg])
+		lg++;
+
+	add_var = write(fd, text_content, lg);
+	close(fd);
+	if (add_var == -1)
+		return (-1);
+
+	return (1);
+}
