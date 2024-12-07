3dc46d2d6e69530633acf172b5ed70ca9171fe13
KillianRipoche <killian.ripoche@gmail.com>
2024-12-07 19:48:43 +0100

    merci Gwendal

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,18 +1,18 @@
 #include "main.h"
+
 /**
- * append_text_to_file - Entry
- * @filename: file add
- * @text_content: texte a ajouter
- * Return: 1
- */
+* append_text_to_file - Entry
+* @filename: name of the file.
+* @text_content: end of the file
+*
+* Return: return 1
+*/
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd; /* file descriptor */
-	int lg = 0; /* longueur de text content */
-	int add_var;
+	 int fd, len = 0, write_status;
 
 	if (filename == NULL)
-		return(-1);
+		return (-1);
 
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
@@ -24,13 +24,13 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (1);
 	}
 
-	while (text_content[lg])
-		lg++;
+	while (text_content[len])
+		len++;
 
-	add_var = write(fd, text_content, lg);
+	write_status = write(fd, text_content, len);
 	close(fd);
 
-	if (add_var == -1)
+	if (write_status == -1)
 		return (-1);
 
 	return (1);
